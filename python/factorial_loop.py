5a8996a08ce5a7bae3486fd0c64bc1b7b86ddf46
Andy Wood <andy@Vimes.local>
2025-05-08 15:14:03 +0100

    feat: add logging

--- python/factorial_loop.py
+++ python/factorial_loop.py
@@ -10,23 +10,42 @@ import logging
 
 N = 5
 
-def args():
+
+def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-q', '--quiet', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
+
+def configure_logging(args):
+    log_level = logging.INFO
+    if args.quiet:
+        log_level = logging.WARNING
+    elif args.verbose:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(level=log_level, format='%(levelname)s: %(message)s')
+
+
 def factorial(n):
+    logging.debug('Calculating factorial of %d' % n)
+
     if n < 0:
+        logging.error('Cannot calculate factorial of a negative number!')
         raise ValueError('Invalid value %d - n must be positive' % n)
 
     result = 1
     for i in range(1, n + 1):
         result *= i
+        logging.debug('Calculated factorial of %d: %d' % (i, result))
 
     return result
 
+
 if __name__ == '__main__':
-    args = args()
+    args = parse_args()
+    configure_logging(args)
+
     answer = factorial(N)
     print('Result = %d' % answer)
