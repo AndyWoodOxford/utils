cac2249418b4c24835e8752a88fc83f8760b4a9b
Andy Wood <andy@Vimes.local>
2025-05-08 14:30:24 +0100

    feat: initiate logging

--- python/factorial_loop.py
+++ python/factorial_loop.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+Calculates the factorial of a non-negative integer. The initial implementation
+is hard-wired to a fixed integer. The algorithm uses a for-loop.
+"""
+
+import argparse
+import logging
+
 N = 5
 
 def args():
-    pass
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-q', '--quiet', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    return parser.parse_args()
 
 def factorial(n):
     if n < 0:
@@ -16,5 +27,6 @@ def factorial(n):
     return result
 
 if __name__ == '__main__':
+    args = args()
     answer = factorial(N)
     print('Result = %d' % answer)
