c6241d7658e9fd51191ef360794b869931c57a02
Andy Wood <andy@Vimes.local>
2025-05-12 12:20:59 +0100

    qa: more unit tests

--- python/tests/test_prime_factors.py
+++ python/tests/test_prime_factors.py
@@ -10,3 +10,24 @@ def test_prime():
 def test_non_prime():
     n = 51
     assert not prime_factors.is_prime(n)
+
+def test_factoring_prime():
+    n = 67
+    factors = prime_factors.prime_factors(n)
+    assert not factors
+
+def test_factoring_distinct_factors():
+    n = 255 # 3 * 5 * 17
+    factors = prime_factors.prime_factors(n)
+    assert len(factors) == 3
+    assert 3 in factors and 5 in factors and 17 in factors
+
+def test_factoring_nondistinct_factors():
+    n = 45 # 3 * 3 * 5
+    factors = prime_factors.prime_factors(n)
+    assert factors.count(3) == 2 and  5 in factors
+
+def test_factoring_exponent():
+    n = 1024 # 2^10
+    factors = prime_factors.prime_factors(n)
+    assert len(factors) == 10 and factors.count(2) == 10
