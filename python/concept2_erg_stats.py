f0c556facc8ec498a6034f5afc20a1aac5a890ca
Andy Wood <andy@Vimes.local>
2025-08-16 12:46:23 +0100

    wip: checking format of splits

--- python/concept2_erg_stats.py
+++ python/concept2_erg_stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 """
-Tabulates 2K and 5K times for a list of 500m splits.
+Tabulates 2K and 5K times for a list of 500m splits. The high and low splits are entered using
+the format 'm:ss' or 'm:ss.x' and must both be between 1 and 4 minutes.
 """
 
 import argparse
 import logging
-from fractions import Fraction
+import re
 
 HIGH_SPLIT = "2:30"
 LOW_SPLIT = "1:30"
@@ -26,9 +27,31 @@ def configure_logging(args):
 
     logging.basicConfig(level=log_level, format='%(levelname)s:%(message)s')
 
+def verify_split(split):
+    print('SPLIT %s' % split)
+    pattern = re.compile('^(\\d){1}:(\\d){1,2}$')
+    if not re.match(pattern, split):
+        print('FAILED MATCH')
+        raise ValueError('The string "%s" is not a valid format for a split' % split)
+    print('PASSED MATCH')
+    minutes = int(split.split(':')[0])
+    print('MINUTES %d' % minutes)
+    if minutes < 1:
+        print('TOO LOW')
+        raise ValueError('The split of "%s" is too low' % split)
+    elif minutes >=4:
+        print('TOO HIGH')
+        raise ValueError('The split of "%s" is too low' % split)
+    print('VERIFY OK')
+
 def convert_split_to_seconds(split):
+    verify_split(split)
     logging.debug('Converting split %s into seconds.' % split)
-    return 0
+    minutes = int(split.split(':')[0])
+    seconds = float(split.split(':')[1])
+    print('SECONDS ARE %.1f' % seconds)
+    print('TOTAL %.1f seconds' % (minutes * 60 + seconds))
+    return minutes * 60 + seconds
 
 def tabulate_times(high_split, low_split, increment = 1.0):
     logging.debug('Tabulating times for splits between %s to %s in increments of %.1f second(s).' % (high_split, low_split, increment))
@@ -38,5 +61,7 @@ def tabulate_times(high_split, low_split, increment = 1.0):
 if __name__ == '__main__':
     args = parse_args()
     configure_logging(args)
+
+
 
     tabulate_times(HIGH_SPLIT, LOW_SPLIT)
--- python/tests/test_concept2_erg.py
+++ python/tests/test_concept2_erg.py
@@ -4,6 +4,26 @@ import pytest
 
 import concept2_erg_stats
 
+def test_invalid_split():
+    split = "split"
+    with pytest.raises(ValueError):
+        concept2_erg_stats.verify_split(split)
+
+def test_invalid_split_missing_seconds():
+    split = "2"
+    with pytest.raises(ValueError):
+        concept2_erg_stats.verify_split(split)
+
+def test_split_too_low():
+    split = "0:59"
+    with pytest.raises(ValueError):
+        concept2_erg_stats.verify_split(split)
+
+def test_split_too_high():
+    split = "4:01"
+    with pytest.raises(ValueError):
+        concept2_erg_stats.verify_split(split)
+
 def test_exact_minute_split_to_seconds():
     split = "2:00"
     expected_value = 120
@@ -24,13 +44,7 @@ def test_typical_split_to_seconds():
     expected_value = 105
     assert concept2_erg_stats.convert_split_to_seconds(split) == expected_value
 
-def test_split_too_low():
-    split = "0:59"
-    with pytest.raises(ValueError):
-        concept2_erg_stats.convert_split_to_seconds(split)
-
-def test_split_too_high():
-    split = "4:01"
-    with pytest.raises(ValueError):
-        concept2_erg_stats.convert_split_to_seconds(split)
-
+def test_decimal_point_split_to_seconds():
+    split = "1:50.5"
+    expected_value = 110.5
+    assert concept2_erg_stats.convert_split_to_seconds(split) == expected_value
