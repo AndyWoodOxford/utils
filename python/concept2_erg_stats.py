ae814ccc0438f8ad041e86bae299a0c19650a7ab
Andy Wood <andy@Vimes.local>
2025-08-18 17:55:19 +0100

    feat: add args for the splits (not yet wired up)

--- python/concept2_erg_stats.py
+++ python/concept2_erg_stats.py
@@ -1,27 +1,64 @@
 #!/usr/bin/env python3
 
 """
-Tabulates 2K and 5K times for a list of 500m splits. The high and low splits are entered using
-the format 'm:ss' or 'm:ss.x' and must both be between 1 and 4 minutes.
+Tabulates 2K and 5K times for a sequence of 500m split times.
 """
 
 import argparse
 import logging
 import re
 
-# TODO make these configurable
-HIGH_SPLIT = "2:01"
-LOW_SPLIT = "1:50"
+DEFAULT_HIGH_SPLIT = "2:15"
+DEFAULT_LOW_SPLIT = "1:45"
+DEFAULT_SPLIT_INCREMENT = 1
 
 DISTANCES = [2000, 5000]
 
 COLUMN_WIDTH = 13
 
+def default_high_split():
+    return DEFAULT_HIGH_SPLIT
+
+def default_low_split():
+    return DEFAULT_LOW_SPLIT
+
+def default_split_increment():
+    return DEFAULT_SPLIT_INCREMENT
+
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-q', '--quiet', help='Quiet mode', action='store_true')
-    parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
+    parser.add_argument(
+        '-H', '--high-split',
+        metavar='SPLIT',
+        help='High 500m split (i.e. slowest pace), enter as e.g. "2:15", "2:20.5" etc',
+        action='store',
+        default=default_high_split()
+    )
+    parser.add_argument(
+        '-I', '--split-increment',
+        metavar='INCR',
+        help = 'Split increment in seconds, enter as e.g. "1", "1.5" etc',
+        action='store',
+        default=default_split_increment()
+    )
+    parser.add_argument(
+        '-L', '--low-split',
+        metavar='SPLIT',
+        help='Low 500m split (i.e. fastest pace), enter as e.g. "2:00", "1:50.5" etc',
+        action='store',
+        default=default_low_split()
+    )
+    parser.add_argument(
+        '-q', '--quiet',
+        help='Quiet mode',
+        action='store_true')
+    parser.add_argument(
+        '-v', '--verbose',
+        help='Verbose mode',
+        action='store_true')
     return parser.parse_args()
+
+
 
 def configure_logging(args):
     log_level = logging.INFO
