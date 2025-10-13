f14e63d6b2bf712cde161a470ac46589a71d23ff
Andy Wood <andy@Vimes.local>
2025-10-13 13:49:53 +0100

    docs: explain what is happening

--- python/prime_factors.py
+++ python/prime_factors.py
@@ -32,7 +32,7 @@ def configure_logging(local_args):
 
 def is_prime(number):
     factor = 2
-    # Commutativity - nudge up potential factors until the square root is reached
+    # Commutativity - keep incrementing potential factors until the square root is reached
     while factor < number and pow(factor, 2) <= number:
         if number % factor == 0:
             return False
@@ -46,17 +46,17 @@ def prime_factors(number):
     dividend = number
     divisor = 2
     while True:
-        # All integers are divisible by 1 so the factoring is now complete
+        # Exit condition 1 - all integers are divisible by 1 so the factoring is now complete
         if dividend == 1:
             logging.debug('Dividend is now 1 - prime factoring is complete')
             break
 
-        # The number is a prime if there are no factors found and the divisor is past the square root
+        # Exit condition 2 - the number is a prime if no factors have been found *and* the divisor exceeds its square root
         if not factors_set and pow(divisor, 2) > number:
             logging.debug('No factors have been found and the divisor %d is larger than the square root of %d - quitting loop' % (divisor, number))
             break
 
-        # If the divisor is prime and is a factor then capture and retry
+        # If the divisor is prime *and* is a factor then capture the divisor, apply against the dividend and retry
         logging.debug('Trying divisor %d' % divisor)
         if is_prime(divisor):
             logging.debug('  Divisor is a prime')
@@ -68,7 +68,7 @@ def prime_factors(number):
                 logging.debug('  Dividend is now %d' % dividend)
                 continue
 
-        # Otherwise try the next integer
+        # The current divisor is not a factor - increment and continue
         divisor += 1
 
     return factors_set
@@ -76,12 +76,13 @@ def prime_factors(number):
 
 def output_string(factors):
     index_list = (range(len(factors)))
+    # e.g. n=40; zip((0,1,2,3), (2,2,2,5)) => {0:2, 1:2, 2:2, 3:5}
     factors_indexed = {index: factor for index, factor in zip(index_list, factors)}
 
     colorama.init(autoreset=True)
+    output = ''
 
     index = 0
-    output = ''
     while index < len(factors):
         prime_factor = factors[index]
         # Unique prime factor - capture and increment to the following one
